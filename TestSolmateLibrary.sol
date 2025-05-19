9cfa2d7c14b062233076ca17c9561ccfa19b1f96
Onu Daniel Onyebuchi <onudanielonyebuchi@gmail.com>
2025-05-19 09:25:18 +0100

    Create TestSolmateLibrary.sol

---
+++ TestSolmateLibrary.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "solmate/tokens/ERC20.sol";
+
+contract SolmateToken is ERC20 {
+    constructor() ERC20("SolmateToken", "SMT", 18) {
+        _mint(msg.sender, 1_000_000 ether);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
